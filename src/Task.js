a7264827f7fb764a38f0079c8c62495522edaa05
Ahmad Rizqi Darmawan <80834548+syreboy@users.noreply.github.com>
2022-12-17 18:02:54 +0700

    this is a commit

--- src/Task.js
+++ src/Task.js
@@ -1,4 +1,4 @@
-const url = "YOURDEPLOYURL" // TODO: replace this
+const url = "https://assignment-deplot-appgallery.netlify.app/" // TODO: replace this
 const studentName = "Ahmad Rizqi Darmawan" // TODO: replace this
 const studentId = "FE2625397" // TODO: replace this
 
